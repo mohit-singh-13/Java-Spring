676746f583155fd25d0f24023704f1bfee0d98be
mohit-singh-13 <mohitsingh092003@gmail.com>
2025-08-30 18:29:34 +0530

    Prepared Statements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,6 @@ import java.sql.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // import package - Done Above ✅
-
-        // load and register (optional now, required before) ✅
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException err) {
@@ -19,35 +16,19 @@ public class Main {
             String url = "jdbc:postgresql://localhost:5432/demo";
             String uname = "postgres";
             String password = "password";
-            // String sqlQuery = "SELECT sname FROM students WHERE id = 1";
-            String sqlQuery = "SELECT * FROM students";
+            String sqlQuery = "INSERT INTO students (id, sname, marks) VALUES (?, ?, ?)";
 
-            // create connection ✅
             Connection connection = DriverManager.getConnection(url, uname, password);
 
-            System.out.println("Connection established");
+            PreparedStatement pStatement = connection.prepareStatement(sqlQuery);
 
-            // create statement ✅
-            Statement statement = connection.createStatement();
+            pStatement.setInt(1, 1002);
+            pStatement.setString(2, "Mohit");
+            pStatement.setInt(3, 60);
 
-            // execute statement ✅
-            ResultSet result = statement.executeQuery(sqlQuery);
+            pStatement.execute();
 
-            // process the results ✅
-            // result.next();
-            // String name = result.getString("sname");
-
-            // System.out.println("Name of the student is " + name);
-
-            while (result.next()) {
-                System.out.print(result.getInt(1) + " - ");
-                System.out.print(result.getString(2) + " - ");
-                System.out.println(result.getInt(3));
-            }
-
-            // close the connection ✅
             connection.close();
-            System.out.println("Connection closed");
         } catch (SQLException err) {
             System.out.println("Error while creating connection : " + err.getMessage());
         }
