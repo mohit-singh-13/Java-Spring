521f46751c071aa0eeaa64327ab30ae2297096f6
mohit-singh-13 <mohitsingh092003@gmail.com>
2025-09-02 20:55:07 +0530

    Fetch data from database using Hibernate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,11 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
-        s1.setsName("Mohit");
-        s1.setRollNo(27);
-        s1.setsAge(22);
+        s1.setsName("Ajay");
+        s1.setRollNo(45);
+        s1.setsAge(21);
+
+        Student s2;
 
         Configuration configuration = new Configuration();
         configuration.addAnnotatedClass(Student.class);
@@ -20,12 +22,11 @@ public class Main {
         try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
             Session session = sessionFactory.openSession();
 
-            // We need to create a transaction before writing anything to DB
-            Transaction transaction = session.beginTransaction();
-            session.persist(s1);
-            transaction.commit();
+            s2 = session.find(Student.class, 27);
 
-            System.out.println(s1);
+            session.close();
+
+            System.out.println(s2);
         } catch (HibernateException err) {
             System.out.println("Error while creating hibernate session : " + err.getMessage());
         }
